0073fca09263fd8e7dc6a58fed0084754110ddfc
vasga-floze <gabriela.floze@gmail.com>
2023-02-27 17:39:15 -0600

    delete the default component, creates a new view and add the route in index.js

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,8 @@
 </nav>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/posts">Posts</router-link>
   </nav>
   <router-view/>
 </template>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus eaque magnam consequatur! Doloremque consequuntur adipisci culpa nemo, optio labore maxime, earum amet molestias rerum delectus, natus tempora magni dolorum pariatur!</p>
   </div>
 </template>
