9bf0d53ffd15a4d02f2d034c29a55df5168a33f0
vasga-floze <gabriela.floze@gmail.com>
2023-02-27 17:53:18 -0600

    add a new component for navbar and replace the native navbar

---
+++ src/components/NavComp.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <router-link to="/" class="navbar-brand" >SOCIAL NET</router-link>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <router-link to="/" class="nav-link active" aria-current="page" >Home</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/about" class="nav-link" >About</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/posts" class="nav-link" >Posts</router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
